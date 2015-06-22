bff568cd6747162c5939ecdd7ff62d373f3595cf
Loek van Gent <loek@1procentclub.nl>
2015-06-22 10:47:32 +0200

    Initial code

---
+++ addon/lib/lex.js
@@ -0,0 +1,46 @@
+'use strict';
+
+var Lexer = require('lex');
+
+function extract(lexeme) {
+    var str = '',
+        quoteChar = null;
+
+    for (var i = 0, len = lexeme.length; i < len; i++) {
+        var c = lexeme[i];
+
+        if (!quoteChar && /["'`]/.test(c)) {
+            quoteChar = c;
+            continue;
+        }
+
+        if (c === quoteChar && lexeme.charAt(i - 1) !== '\\') break;
+        if (quoteChar) str = str + c;
+    }
+
+    return str;
+}
+
+function unescape(str) {
+    return str.replace(/\\'/, '\'');
+}
+
+module.exports = function tokenize(source) {
+    var tokens = [],
+        lexer = new Lexer(function() {});
+
+    lexer.addRule(/\{\{t.+?\s*\}\}/g, function(lexeme) {
+        var str = extract(lexeme);
+        tokens.push(unescape(str));
+    }, []);
+
+    lexer.addRule(/t\`.+?\`/g, function(lexeme) {
+        var str = extract(lexeme);
+        tokens.push(unescape(str));
+    }, []);
+
+    lexer.setInput(source);
+    lexer.lex();
+
+    return tokens;
+};
